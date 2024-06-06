906aaafd79ecd1c16c1b8cee43e80e1ace4d0215
szymic <szymon.julian.michalowski@gmail.com>
2024-06-06 10:49:01 +0200

    final version

--- src/radix4.py
+++ src/radix4.py
@@ -1,5 +1,5 @@
 import time
-
+import random
 
 def int_to_bits(n, width):
     if width % 2 != 0:
@@ -12,20 +12,28 @@ def int_to_bits(n, width):
     return bits[::-1]
 
 def bits_to_int(bits):
+    n = len(bits)
     result = 0
-    for i, bit in enumerate(bits):
-        result += bit << i
+    for i in range(n):
+        result += bits[i] << i
+
+    # Sprawdź, czy liczba jest ujemna (najstarszy bit ustawiony na 1)
+    if bits[-1] == 1:
+        result -= (1 << n)
+
     return result
 
 def booth_radix4_full(x, y):
+    # zamiana mnożnika na U2
     bit_length = max(x.bit_length(), y.bit_length()) + 1
     bit_length = (bit_length + 1) // 2 * 2
-    x_bits = int_to_bits(x, bit_length)
     y_bits = int_to_bits(y, bit_length)
     w = (len(y_bits) + 1) // 2
 
+    # tablica produktów częsciowych (PPA)
     partial_products = []
 
+    # Podzial mnoznika na w=ceil(n/2) grup i wyznaczenie wartości di
     for i in range(w):
         overlap_bit = y_bits[2 * i - 1] if (2 * i - 1) >= 0 else 0
         di = -2 * y_bits[2 * i + 1] + y_bits[2 * i] + overlap_bit
@@ -41,6 +49,7 @@ def booth_radix4_full(x, y):
         elif di == -2:
             partial_product = -(x << 1)
 
+        # Dodanie wyznaczonego iloczynu częściowego do PPA
         partial_product_shifted = partial_product << (2 * i)
         partial_product_bits = int_to_bits(partial_product_shifted, 2 * bit_length)
         partial_products.append(partial_product_bits)
@@ -48,6 +57,7 @@ def booth_radix4_full(x, y):
     result_bits = [0] * (2 * bit_length)
     carry = 0
 
+    # Jednostka sumująca
     for bit_position in range(2 * bit_length):
         column_sum = carry
         for pp in partial_products:
@@ -59,22 +69,20 @@ def booth_radix4_full(x, y):
 
     result = bits_to_int(result_bits)
 
-    # Correct the sign if the result is negative
-    if result_bits[-1] == 1:
-        result -= (1 << (2 * bit_length))
-
     return result
 
 
 def booth_radix4_fixed(x, y):
+    # zamiana mnożnika na U2
     bit_length = max(x.bit_length(), y.bit_length()) + 1
     bit_length = (bit_length + 1) // 2 * 2
-    x_bits = int_to_bits(x, bit_length)
     y_bits = int_to_bits(y, bit_length)
     w = (len(y_bits) + 1) // 2
 
+    # tablica produktów częściowych (PPA)
     partial_products = []
 
+    # Podział mnoznika na w=ceil(n/2) grup i wyznaczenie wartości di
     for i in range(w):
         overlap_bit = y_bits[2 * i - 1] if (2 * i - 1) >= 0 else 0
         di = -2 * y_bits[2 * i + 1] + y_bits[2 * i] + overlap_bit
@@ -94,20 +102,19 @@ def booth_radix4_fixed(x, y):
         partial_product_bits = int_to_bits(partial_product_shifted, 2 * bit_length)
         partial_products.append(partial_product_bits)
 
-    # Wynik o dlugosci 2n (mlodsze n - same 0)
+
     result_bits = [0] * (2 * bit_length)
     carry = 0
 
-    # Wyliczamy przeniesienie z mlodszych n bitow
+    # Propagacja przeniesienia z młodszych n bitów
     for bit_position in range(bit_length):
         column_sum = carry
-        # dla kazdego iloczynu czesciowego:
         for pp in partial_products:
             column_sum += pp[bit_position]
 
         carry = column_sum // 2
 
-    # Sumowanie starszych n bitow (z propagowanym przeniesieniem)
+    # Jednostka sumująca - sumowanie starszych n bitow (z propagowanym przeniesieniem)
     for bit_position in range(bit_length, 2 * bit_length):
         column_sum = carry    # dla n+1 bitu
         for pp in partial_products:
@@ -117,15 +124,14 @@ def booth_radix4_fixed(x, y):
         result_bits[bit_position] = column_sum % 2
         carry = column_sum // 2
 
-    result = bits_to_int(result_bits)
 
-    # Correct the sign if the result is negative
-    if result_bits[-1] == 1:
-        result -= (1 << (2 * bit_length))
+    result = bits_to_int(result_bits)
 
     return result
 
 
+
+"""
 # Testowanie
 x = -7
 y = 12
@@ -151,3 +157,34 @@ for i in range(100):
 
 print("Sredni czas mnozenia radix-8 fixed-width dla 100 pomiarow: ", time_in_micro / 100.0)
 print("Wynik:", result_fixed)
+"""
+
+def pomiary_radix_4(n):
+
+    # Losowe operandy n-bitowe (w biarnym n-1-bitowe, wtedy przy konwersji na U2 - n-bitowe)
+    x_bits = [0] * n
+    y_bits = [0] * n
+    time_in_micro = 0
+
+    # 100 pomiarow
+    for i in range(100):
+
+        # Najpierw generujemy dwa operandy o dlugosci n bitow w U2
+        for bit in range(n):
+            x_bits[bit] = random.randrange(2)
+            y_bits[bit] = random.randrange(2)
+
+        # Zamiana na int
+        x = bits_to_int(x_bits)
+        y = bits_to_int(y_bits)
+
+        start_time = time.perf_counter()
+        booth_radix4_fixed(x, y)
+        end_time = time.perf_counter()
+
+        time_in_micro += (end_time - start_time) * (10 ** 6)
+
+    print(f"Sredni czas mnozenia radix-4 fixed-width (post-truncated) dla 100 pomiarow, dla {n}-bitowych operandow: {round(time_in_micro / 100.0, 2)}")
+
+
+pomiary_radix_4(640)
--- src/radix8.py
+++ src/radix8.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 def half_adder(bit1, bit2):
@@ -22,20 +23,31 @@ def int_to_bits(n, width):
     bits = [int(b) for b in f'{n:0{width}b}']
     return bits[::-1]
 
+
 def bits_to_int(bits):
+    n = len(bits)
     result = 0
-    for i, bit in enumerate(bits):
-        result += bit << i
+    for i in range(n):
+        result += bits[i] << i
+
+    # Sprawdź, czy liczba jest ujemna (najstarszy bit ustawiony na 1)
+    if bits[-1] == 1:
+        result -= (1 << n)
+
     return result
 
+
 def booth_radix8_full(x, y):
+    # zamiana mnożnika na U2
     bit_length = max(x.bit_length(), y.bit_length()) + 1
     bit_length = (bit_length + 2) // 3 * 3
     y_bits = int_to_bits(y, bit_length)
     w = (len(y_bits) + 2) // 3
 
+    # tablica produktów częsciowych (PPA)
     partial_products = []
 
+    # Podzial mnoznika na w grup i wyznaczenie wartości di
     for i in range(w):
         overlap_bit = y_bits[3 * i - 1] if (3 * i - 1) >= 0 else 0
         di = -4 * y_bits[3 * i + 2] + 2 * y_bits[3 * i + 1] + y_bits[3 * i] + overlap_bit
@@ -59,6 +71,7 @@ def booth_radix8_full(x, y):
         elif di == -4:
             partial_product = -(x << 2)
 
+        # Dodanie wyznaczonego iloczynu częściowego do PPA
         partial_product_shifted = partial_product << (3 * i)
         partial_product_bits = int_to_bits(partial_product_shifted, 2 * bit_length)
         partial_products.append(partial_product_bits)
@@ -67,6 +80,7 @@ def booth_radix8_full(x, y):
     result_bits = [0] * (2 * bit_length)
     carry = 0
 
+    # Jednostka sumująca
     for bit_position in range(2 * bit_length):
         column_sum = carry
         for pp in partial_products:
@@ -79,21 +93,20 @@ def booth_radix8_full(x, y):
 
     result = bits_to_int(result_bits)
 
-    # Correct the sign if the result is negative
-    if result_bits[-1] == 1:
-        result -= (1 << (2 * bit_length))
-
     return result
 
 
 def booth_radix8_fixed(x, y):
+    # zamiana mnożnika na U2
     bit_length = max(x.bit_length(), y.bit_length()) + 1
     bit_length = (bit_length + 2) // 3 * 3
     y_bits = int_to_bits(y, bit_length)
     w = (len(y_bits) + 2) // 3
 
+    # tablica produktów częsciowych (PPA)
     partial_products = []
 
+    # Podzial mnoznika na w grup i wyznaczenie wartości di
     for i in range(w):
         overlap_bit = y_bits[3 * i - 1] if (3 * i - 1) >= 0 else 0
         di = -4 * y_bits[3 * i + 2] + 2 * y_bits[3 * i + 1] + y_bits[3 * i] + overlap_bit
@@ -117,25 +130,24 @@ def booth_radix8_fixed(x, y):
         elif di == -4:
             partial_product = -(x << 2)
 
+        # Dodanie wyznaczonego iloczynu częściowego do PPA
         partial_product_shifted = partial_product << (3 * i)
         partial_product_bits = int_to_bits(partial_product_shifted, 2 * bit_length)
         partial_products.append(partial_product_bits)
 
 
-    # Wynik o dlugosci 2n (mlodsze n - same 0)
     result_bits = [0] * 2 * bit_length
     carry = 0
 
-    # Wyliczamy przeniesienie z mlodszych n bitow
+    # Generator przeniesienia - propagacja przeniesienia z młodszych n bitów
     for bit_position in range(bit_length):
         column_sum = carry
-        # dla kazdego iloczynu czesciowego:
         for pp in partial_products:
             column_sum += pp[bit_position]
 
         carry = column_sum // 2
 
-    # Sumowanie starszych n bitow (z propagowanym przeniesieniem)
+    # Jednostka sumująca - sumowanie starszych n bitow (z propagowanym przeniesieniem)
     for bit_position in range(bit_length, 2 * bit_length):
         column_sum = carry  # dla n+1 bitu
         for pp in partial_products:
@@ -147,13 +159,10 @@ def booth_radix8_fixed(x, y):
 
     result = bits_to_int(result_bits)
 
-    # Correct the sign if the result is negative
-    if result_bits[-1] == 1:
-        result -= (1 << (2 * bit_length))
-
     return result
 
 
+"""
 # Test algorytmu z przykładowymi wartościami
 x = 7  # Mnożna
 y = 12  # Mnożnik
@@ -180,3 +189,33 @@ for i in range(100):
 
 print("Sredni czas mnozenia radix-8 fixed-width (post-truncated) dla 100 pomiarow: ", time_in_micro / 100.0)
 print("Wynik:", result_fixed)
+"""
+
+def pomiary_radix_8(n):
+
+    # Losowe operandy n-bitowe (w biarnym n-1-bitowe, wtedy przy konwersji na U2 - n-bitowe)
+    x_bits = [0] * n
+    y_bits = [0] * n
+    time_in_micro = 0
+
+    # 100 pomiarow
+    for i in range(100):
+
+        # Najpierw generujemy dwa operandy o dlugosci n bitow w U2
+        for bit in range(n):
+            x_bits[bit] = random.randrange(2)
+            y_bits[bit] = random.randrange(2)
+
+        # Zamiana na int
+        x = bits_to_int(x_bits)
+        y = bits_to_int(y_bits)
+
+        start_time = time.perf_counter()
+        booth_radix8_fixed(x, y)
+        end_time = time.perf_counter()
+
+        time_in_micro += (end_time - start_time) * (10 ** 6)
+
+    print(f"Sredni czas mnozenia radix-8 fixed-width (post-truncated) dla 100 pomiarow, dla {n}-bitowych operandow: {round(time_in_micro / 100.0, 2)}")
+
+pomiary_radix_8(80)
